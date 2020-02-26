f01b02819ccea8c35771edad1e22f9eb22bc549c
Johnathan Pruitt <pruittj2000@yahoo.com>
2020-02-26 01:35:58 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,25 @@
+require 'pp'
+
 def roll_call_dwarves(array)
-  array.each_with_index{|dwarf,index| puts "#{index + 1}. #{dwarf}"}
+  pp array.each_with_index{|dwarf,index| puts "#{index + 1}. #{dwarf}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.collect{|word| word = word.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.any? {|word| word.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+
+  cheese_types.each do |cheese|
+    if array.any?{|item| item == cheese}
+      return cheese
+    end
+  end
+  nil
 end
